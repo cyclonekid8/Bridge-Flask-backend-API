91faa357ec8bef3091dc5e68f06e1a346cd76991
cyclonekid8 <muzafarshahmarican@gmail.com>
2021-04-14 00:21:45 +0800

    3rd commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,jsonify,request,Response
-from flask_cors import CORS, cross_origin
+
 app=Flask(__name__)
 def format_server_time():
   server_time = time.localtime()
   return time.strftime("%I:%M:%S %p", server_time)
-CORS(app, support_credentials=True)
+
 if __name__ == '__main__':
     app.debug=True
     app.run(host='0.0.0.0',port=int(os.environ.get('PORT',8080)))
